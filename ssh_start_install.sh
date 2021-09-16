ecd73f45530426ec8f7783d0f1b1a1045a58bf10
sjh-0504 <63689383+sjh-0504@users.noreply.github.com>
2021-09-16 15:03:25 +0800

    Update ssh_start_install.sh

--- ssh_start_install.sh
+++ ssh_start_install.sh
@@ -7,18 +7,19 @@ ip=$(grep -oP '([0-9]{1,3}\.){1,3}[0-9]{1,3}' <<< $con)
 arr=(${ip//,/})
 
 echo ${arr[@]}
+load="/vagrant/peiz/$1"
 
-mkdir /vagrant/peiz/$1
+mkdir $load
 
 for n in ${arr[@]};do
 	./wireguard_install $n
 	sleep 40
 	echo -e "done\n"
-	scp $MYUSER@$n:/etc/wireguard/client.conf /vagrant/peiz/$1/$n.conf
+	scp $MYUSER@$n:/etc/wireguard/client.conf $load/$n.conf
 done
 
 for m in ${arr[@]};do
 	if [ -r "/vagrant/peiz/$1/$m.conf" ];then
-		scp $MYUSER@$m:/etc/wireguard/client.conf /vagrant/peiz/$1/$m.conf
+		scp $MYUSER@$m:/etc/wireguard/client.conf $load/$m.conf
 	fi	
 done
